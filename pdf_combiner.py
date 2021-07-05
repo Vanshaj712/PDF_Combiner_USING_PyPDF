f6f75303316a29352e27a7f9df5596a6f7bbd4eb
Vanshaj712 <85443589+Vanshaj712@users.noreply.github.com>
2021-07-05 22:06:10 +0530

    Update pdf_combiner.py

--- pdf_combiner.py
+++ pdf_combiner.py
@@ -15,21 +15,21 @@ pdf2Reader = PyPDF2.PdfFileReader(pdf2)
 # Create a new PdfFileWriter object which represents a blank PDF document
 writer = PyPDF2.PdfFileWriter()
 
-# Loop through all the pagenumbers for the first document
+# Looping through all the pagenumbers for the first document
 for pageNum in range(pdf1Reader.numPages):
     pageObj = pdf1Reader.getPage(pageNum)
     writer.addPage(pageObj)
 
-# Loop through all the pagenumbers for the second document
+# Looping through all the pagenumbers for the second document
 for pageNum in range(pdf2Reader.numPages):
     pageObj = pdf2Reader.getPage(pageNum)
     writer.addPage(pageObj)
 
-# Now that you have copied all the pages in both the documents, write them into the a new document
+# We have copied all the pages in both the documents. Write them into the a new document
 pdfOutputFile = open('MergedFiles.pdf', 'wb')
 writer.write(pdfOutputFile)
 
-# Close all the files - Created as well as opened
+# Close all the files 
 pdfOutputFile.close()
 pdf1.close()
 pdf2.close()
